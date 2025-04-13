6fdf440dc1839112426c8877d731c6e2131e9900
JairRivera14 <jairrivera536@gmail.com>
2025-04-13 16:28:59 -0600

    Update act3JERC_.tf

--- act3JERC_.tf
+++ act3JERC_.tf
@@ -111,7 +111,7 @@ resource "aws_security_group" "web_sg" {
 
 # --- Jump Server (Linux) ---
 resource "aws_instance" "jump_server" {
-  ami                    = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2
+  ami                    = "ami-00a929b66ed6e0de6" # Amazon Linux 2
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.subnet_public.id
   vpc_security_group_ids = [aws_security_group.jump_sg.id]
@@ -126,7 +126,7 @@ resource "aws_instance" "jump_server" {
 # --- Web Servers (Linux x3) ---
 resource "aws_instance" "web_server" {
   count                  = 4
-  ami                    = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2
+  ami                    = "ami-00a929b66ed6e0de6" # Amazon Linux 2
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.subnet_public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
